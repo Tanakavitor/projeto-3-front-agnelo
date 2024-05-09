4231c3e70016cc5259605b3fd9f4db9a589024de
Vitor <seuemail>
2024-05-09 11:11:32 -0300

    criando a tela de login

---
+++ notes-frontend/components/Login/index.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import axios from 'axios';
+import './style.css';
+
+export default function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const login = (event) => {
+        event.preventDefault();
+
+        const data = {
+            username: username,
+            password: password
+        };
+
+        axios.post('http://localhost:8000/api/token/', data)
+        .then((response) => {
+            const token = response.data.token;
+            localStorage.setItem('token', token);
+        })
+        .catch((error) => {
+            console.error("There was an error!", error);
+        });
+    }
+
+
+    return (
+        <div className="login-wrapper">
+            <h1>Login</h1>
+            <form onSubmit={login}>
+                <label>
+                    <p>Username</p>
+                    <input type="text" onChange={e => setUsername(e.target.value)} />
+                </label>
+                <label>
+                    <p>Password</p>
+                    <input type="password" onChange={e => setPassword(e.target.value)} />
+                </label>
+                <div>
+                    <button type="submit">Submit</button>
+                </div>
+            </form>
+        </div>
+    )
+}
