8f214894712b81a99afecf33e6578348ee44f40d
Leonardo <leonardoar2@al.insper.edu.br>
2024-05-14 09:11:47 -0300

    ...

--- notes-frontend/components/MyNotes/index.jsx
+++ notes-frontend/components/MyNotes/index.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useLoaderData, useNavigate } from "react-router-dom";
 
 export default function MyNotes() {
     const [MyNotes, setMyNotes] = useState([]);
+    const navigate = useNavigate();
 
     useEffect(() => {
         axios.get('http://127.0.0.1:8000/api/my_notes/', {
@@ -16,13 +18,18 @@ export default function MyNotes() {
     }, [])
 
     return (
+        <>
         <div>
             <h1>My Notes</h1>
             <ul>
                 {MyNotes.map((note) => (
-                    <li key={note.id}>{note.title}</li>
+                    <li key={note.id}>{note.content}</li>
                 ))}
             </ul>
         </div>
+        <div>
+            <button onClick={() => navigate('/All_Notes')}>Return to Home Page</button>
+        </div>
+        </>
     )
 }
